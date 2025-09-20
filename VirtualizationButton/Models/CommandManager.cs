cd9d6d9154f32aeba966502b02628eaea41217fd
Boris <mr.boriska1998@mail.ru>
2025-09-20 22:53:15 +0300

    Добавьте файлы проекта.

---
+++ VirtualizationButton/Models/CommandManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VirtualizationButton.Models
+{
+    public class CommandManager
+    {
+        public static void EnableVirtualization()
+        {
+            var processInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                Verb = "runas",
+                LoadUserProfile = true,
+                FileName = "powershell.exe",
+                Arguments = "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All",
+                RedirectStandardOutput = false,
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            System.Diagnostics.Process.Start(processInfo);
+            //MessageBox.Show("enable");
+        }
+
+        public static void DisableVirtualization()
+        {
+            var processInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                Verb = "runas",
+                LoadUserProfile = true,
+                FileName = "powershell.exe",
+                Arguments = "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All",
+                RedirectStandardOutput = false,
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            System.Diagnostics.Process.Start(processInfo);
+            //MessageBox.Show("disable");
+        }
+
+        public static bool GetVirtualizationStatus()
+        {
+            Process process2 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command Get-ComputerInfo -Property \"Hyper*\"",
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+            return Convert.ToBoolean(process2.StandardOutput.ReadToEnd().Trim().Split(new char[] { '\r', '\n' })[0].Replace(" ", "").Split(new char[] { ':' })[1].ToLower());
+        }
+    }
+}
---
+++ VirtualizationButton/ViewModels/CommandManagerViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualizationButton.Models;
+
+namespace VirtualizationButton.ViewModels
+{
+    public class CommandManagerViewModel : INotifyPropertyChanged
+    {
+        private bool _isToggleActive ;
+        public bool IsToggleActive
+        {
+            get { return _isToggleActive; }
+            set
+            {
+                _isToggleActive = value;
+                OnPropertyChanged(nameof(IsToggleActive));
+                
+            }
+        }
+        public CommandManagerViewModel()
+        {
+            _isToggleActive = CommandManager.GetVirtualizationStatus();
+            OnPropertyChanged(nameof(IsToggleActive));
+        }
+        private RelayCommand virtualizatuonCommand;
+        public RelayCommand VirtualizationCommand
+        {
+            get
+            {
+                return virtualizatuonCommand ??
+                  (virtualizatuonCommand = new RelayCommand(obj =>
+                  {   
+                      
+                      if(IsToggleActive){
+                          CommandManager.EnableVirtualizatuon();
+                      }
+                      else
+                      {
+                          CommandManager.DisableVirtualizatuon();
+                      }
+                  }));
+            }
+        }
+        // (virtualizatuonCommand = new RelayCommand(obj => IsToggleActive? CommandManager.EnableVirtualizatuon() : CommandManager.DisableVirtualizatuon())); 
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
