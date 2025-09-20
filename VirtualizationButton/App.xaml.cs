f3dbf30df240c4cee910d8fc5339982cee84a84c
Boris <mr.boriska1998@mail.ru>
2025-09-21 02:06:22 +0300

    Checking for administrator rights

--- VirtualizationButton/App.xaml.cs
+++ VirtualizationButton/App.xaml.cs
@@ -9,6 +9,20 @@ namespace VirtualizationButton
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (!Environment.IsPrivilegedProcess)
+            {
+                MessageBox.Show("The program requires administrator rights to run",
+                              "Error",
+                              MessageBoxButton.OK,
+                              MessageBoxImage.Error);
+
+                Application.Current.Shutdown();
+                return;
+            }
+            base.OnStartup(e);
+        }
     }
 
 }
